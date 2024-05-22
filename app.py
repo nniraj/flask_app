8f47355fb90fd57ab3a9e43e2d97cff7aaa61b7e
nniraj <marcosjags@gmail.com>
2024-05-22 13:01:00 +0530

    Updated the code for insertion and retrival of data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template, g
+from flask import Flask, jsonify, request, render_template, g, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
@@ -26,7 +26,7 @@ def viewresults():
     cur = db.execute('select id, name, location from users')
     results = cur.fetchall()
     data = [res['id'] for res in results]
-    return 'The id is {}.'.format(results[0]['id'])
+    return 'The id is {} and name is {} and location is {}.'.format(results[len(data)-1]['id'], results[len(data)-1]['name'], results[len(data)-1]['location'])
 
 @app.route("/<name>")
 def index(name):
@@ -36,10 +36,20 @@ def index(name):
 def theform():
     if request.method == 'GET':
         return render_template('form.html')
+    else:
+        name = request.form['name']
+        location = request.form['location']
+        db = get_db()
+        db.execute('insert into users (name, location) values(?,?)', [name, location])
+        db.commit()
+        return redirect(url_for('home', name=name, location=location))
 
-@app.route("/home/<string:name>", methods=['GET'])
-def home(name):
-    return render_template('home.html', name=name)
+@app.route("/home", methods=['GET'])
+def home():
+    db= get_db()
+    cur = db.execute('Select id, name, location from users')
+    results = cur.fetchall()
+    return render_template('home.html', results=results)
 
 
 @app.route("/jsonify")
